84d5769a25f066dd8a29002ed390785dd0c526fa
alkushelevskiy <alkushelevskiy@ya.ru>
2024-03-16 10:48:48 +0300

    Добавил выделение активной страницы в пагинации

--- src/pages/Posts/components/Pagination/index.tsx
+++ src/pages/Posts/components/Pagination/index.tsx
@@ -1,12 +1,12 @@
 import s from './pagination.module.css'
 import React from "react";
 type paginationProps = {limit:number, length:number, page:number, setPage: (page:number)=>void}
-const PostsList = React.memo((props:paginationProps) => {
+const PostsList = (props:paginationProps) => {
     const PagesButtons = ()=>{
         let pagesCount = Math.ceil(props.length/props.limit)
         let buttons = []
         for(let i=1;i<=pagesCount;i++){
-            buttons.push(<button className={s.button} key={i} onClick={()=>{props.setPage(i)}}>{i}</button>)
+            buttons.push(<button className={[s.button, i === props.page ? s.selected : ''].join(' ')} key={i} onClick={()=>{props.setPage(i)}}>{i}</button>)
         }
         return <div className={s.container}>{buttons}</div>
     }
@@ -16,8 +16,5 @@ const PostsList = React.memo((props:paginationProps) => {
         <PagesButtons />
         </div>
     );
-},(prevProps:Readonly<paginationProps>,nextProps:Readonly<paginationProps>)=>{
-    if(prevProps.length !== nextProps.length) return false
-    else return true
-})
+}
 export default PostsList;
