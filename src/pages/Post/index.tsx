893a3059f55020dfd3625ee863cfa731d2443098
alkushelevskiy <alkushelevskiy@ya.ru>
2024-03-16 10:59:53 +0300

    Добавил loading на страницу поста

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -7,15 +7,24 @@ import {Post} from "types";
 const PostPage: React.FC = ()=>{
     const { id } = useParams<{ id:string }>()
     const [post,setPost] = useState<Post>()
+    const [loading,setLoading] = useState(true)
+
     useEffect(()=>{
         if(id)
-            getPostFetch(id).then((value:Post)=>setPost(value))
+            getPostFetch(id).then((value:Post)=>{
+                setLoading(false)
+                setPost(value)
+            })
     },[id])
+
     return <div className={s.container}>
-        <Link className={s.back_link} to="/posts">Назад</Link>
-        {post && <>
-            <h2 className={s.title}>{post.title}</h2>
-            <div className={s.body}>{post.body}</div>
+        {loading ? <div>Загрузка...</div>
+            : <>
+            <Link className={s.back_link} to="/posts">Назад</Link>
+            {post && <div>
+                <h2 className={s.title}>{post.title}</h2>
+                <div className={s.body}>{post.body}</div>
+            </div>}
         </>}
     </div>
 }
